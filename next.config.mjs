e27b4da05af04e1c8876fa3a9e3c604ac2c239fb
David Pietrocola <pietrocoladavid@gmail.com>
2024-04-04 18:24:26 -0400

    style: linting

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // webpackDevMiddleware: config => {
-  //   config.watchOptions = {
-  //     poll: 1000,
-  //     aggregateTimeout: 300,
-  //   }
-  //   return config
-  // },
+  images: {
+    domains: ["github.com"],
+  },
 };
 
 export default nextConfig;
--- src/components/HeroCards.tsx
+++ src/components/HeroCards.tsx
@@ -1,5 +1,4 @@
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
-import { Badge } from "./ui/badge";
 import { Button, buttonVariants } from "@/components/ui/button";
 import {
   Card,
@@ -9,10 +8,8 @@ import {
   CardTitle,
   CardFooter,
 } from "@/components/ui/card";
-import { Accessibility, Check, GitBranch, Linkedin } from "lucide-react";
-import { LightBulbIcon } from "./Icons";
+import { Check, Linkedin } from "lucide-react";
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
-import Image from "next/image";
 import Link from "next/link";
 
 export const HeroCards = () => {
